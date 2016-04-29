e8bad8e02e0cd99a5c3930f0fc30e940f61f6df6
Emmett Neyman <eneyman@seas.upenn.edu>
2016-04-29 15:45:58 +0000

    fixed color, new html

--- server.py
+++ server.py
@@ -4,20 +4,24 @@ from flask import render_template
 import os
 
 app = Flask(__name__)
-
+color = ""
 @app.route('/')
 def my_form():
 	return render_template("index.html")
 
 @app.route('/', methods=['GET', 'POST'])
 def my_form_post():
+	if 'color' in request.form:
+		global color
+		color = request.form['color']
+		print color
+		return render_template("index.html")
 	if 'program' in request.form:
 		text = request.form['program']
-        	os.system("python write_to_screen.py \"" + text + "\"")
-	if 'color' in request.form:
-		color = request.form['color']
-		os.system("python set_color.py \"" + color + "\"")
-	return render_template("index.html")
+		os.system("pkill -f write_to_screen.py")
+		os.system("python write_to_screen.py \"" + text + "\" " + color + " &")
+		return render_template("index.html")
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run(host="0.0.0.0")
--- write_to_screen.py
+++ write_to_screen.py
@@ -4,19 +4,38 @@ import Adafruit_CharLCD as LCD
 import sys
 import time
 
+print sys.argv[2]
+
 lcd_columns = 16
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
+lcd = LCD.Adafruit_CharLCDPlate()
+if sys.argv[2] == "red":
+    lcd.set_color(1.0, 0.0, 0.0)
+if sys.argv[2] == "green":
+    lcd.set_color(0.0, 1.0, 0.0)
+if sys.argv[2] == "blue":
+    lcd.set_color(0.0, 0.0, 1.0)
+if sys.argv[2] == "yellow":
+    lcd.set_color(1.0, 1.0, 0.0)
+if sys.argv[2] == "cyan":
+    lcd.set_color(0.0, 1.0, 1.0)
+if sys.argv[2] == "magenta":
+    lcd.set_color(1.0, 0.0, 1.0)
+if sys.argv[2] == "white":
+    lcd.set_color(1.0, 1.0, 1.0)
+
 lcd.clear()
 message = sys.argv[1]
 lcd.message(message)
-while(True):
-    for i in range(len(message)-lcd_columns):
-        time.sleep(1)
-        lcd.move_left()
-    for i in range(len(message)-lcd_columns):
-        time.sleep(1)
-        lcd.move_right()
+if lcd_columns < len(message):
+    while(True):
+    	for i in range(len(message)-lcd_columns):
+            time.sleep(1)
+            lcd.move_left()
+    	for i in range(len(message)-lcd_columns):
+            time.sleep(1)
+            lcd.move_right()
 
 
