6b596757e1bda924a7dd526c1837d7d4c5367fe0
Emmett Neyman <eneyman@seas.upenn.edu>
2016-04-29 16:21:28 +0000

    new final

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 color = "white"
+text = "Default Text"
 @app.route('/')
 def my_form():
 	return render_template("index.html")
@@ -14,8 +15,11 @@ def my_form_post():
 	if 'color' in request.form:
 		global color
 		color = request.form['color']
+		os.system("sudo pkill -f write_to_screen.py")
+		os.system("python write_to_screen.py \"" + text + "\" " + color + " &")
 		return render_template("index.html")
 	if 'program' in request.form:
+		global text
 		text = request.form['program']
 		os.system("sudo pkill -f write_to_screen.py")
 		os.system("python write_to_screen.py \"" + text + "\" " + color + " &")
